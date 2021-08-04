1b1f8b61dd486e5f845773aa69a3b06ebe99c04a
Justin Turner <jmturner638@gmail.com>
2021-08-04 02:12:20 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
+from product import Product
+
 def main():
     #write your code below this line
+    banana = Product("Banana", 1.1, 13)
+
+    banana.print_product()
 
 if __name__ == '__main__':
     main()
